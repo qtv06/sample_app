cfa8f2d79620994308c7084abcea7697be2b18b4
vovanquang12cntt <vanquang451@gmail.com>
2017-12-12 15:08:07 +0700

    chapter 4 - rails-flavored

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # return title for per pages
+  def full_title title_page = ""
+    base_title = I18n.t ".base_title"
+    if title_page.empty?
+      base_title
+    else
+      title_page + " | " + base_title
+    end
+  end
 end
