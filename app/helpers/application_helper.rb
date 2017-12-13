747d1529c448bea05fd4f0c68611e63106b5d4b1
vovanquang12cntt <vanquang451@gmail.com>
2017-12-13 11:17:19 +0700

    chapter5-filling-in-layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   # return title for per pages
-  def full_title title_page = ""
+  def full_title title_page
     base_title = I18n.t ".base_title"
-    if title_page.empty?
+    if title_page.blank?
       base_title
     else
       title_page + " | " + base_title
