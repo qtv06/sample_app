464f1cb5d86fee18d7c9532f632d4ff1304a6b8d
vovanquang12cntt <vanquang451@gmail.com>
2017-12-18 08:09:01 +0700

    new controller session and actions as login,logout,sigup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
-  get "users/new"
-  resources "users"
+  get "login" , to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  resources :users
 end
