d1a8f636d3486f05b8d7a1b5d8b82fb32728d626
vovanquang12cntt <vanquang451@gmail.com>
2017-12-18 10:06:02 +0700

    sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,29 @@
 class UsersController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash.now[:danger] = I18n.t "users.flash.not_found"
+    redirect_to root_path
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash.now[:success] = I18n.t "users.flash.success"
+      redirect_to @user
+    else
+      flash.now[:danger] = I18n.t "users.flash.danger"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#home"
-  get "users/new"
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
   get "static_pages/contact"
+  get "users/new"
+  resources "users"
 end
